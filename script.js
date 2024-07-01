4a4007cc89feba6069de9c92b46dd191682f27bc
claygro <166215085+claygro@users.noreply.github.com>
2024-07-01 10:09:40 -0400

    added index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+function show() {
+  document.querySelector("#slide_bar").style.display = "flex";
+
+  document.querySelector(".link").style.display = "none";
+}
+function hide() {
+  document.querySelector(".slide-bar").style.display = "flex";
+}
